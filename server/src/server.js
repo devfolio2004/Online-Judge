866cd28e70b1d32b5f9696820b82d25096df5d8b
Sarthak Ayachi <sarthakmasterayachi@gmail.com>
2026-08-18 18:02:16 +0530

    Connected MongoDB and Mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 import app from "./app.js";
-
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+
+await connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.`);
