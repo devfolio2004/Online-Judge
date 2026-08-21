a0aba97b70636f9d4df9267c21d96ac65cf7c8bf
Sarthak Ayachi <sarthakmasterayachi@gmail.com>
2026-08-21 15:57:15 +0530

    Create Problem Model

---
+++ server/src/models/problem.js
@@ -0,0 +1,110 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const visibleTestCaseSchema = new Schema(
+  {
+    input: {
+      type: String,
+      required: true,
+    },
+    output: {
+      type: String,
+      required: true,
+    },
+    explanation: {
+      type: String,
+      required: true,
+    },
+  },
+  
+);
+
+const hiddenTestCaseSchema = new Schema(
+  {
+    input: {
+      type: String,
+      required: true,
+    },
+    output: {
+      type: String,
+      required: true,
+    },
+  },
+  
+);
+
+const boilerPlateSchema = new Schema(
+  {
+    language: {
+      type: String,
+      required: true,
+      enum: ["C++", "Java", "JavaScript", "Python"],
+    },
+    initialCode: {
+      type: String,
+      required: true,
+    },
+  },
+  
+);
+
+const problemSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    difficulty: {
+      type: String,
+      required: true,
+      enum: ["Easy", "Medium", "Hard"],
+      trim: true,
+    },
+    tags: [
+      {
+        type: String,
+        enum: [
+          "Array",
+          "Graph Theory",
+          "Tree",
+          "Linked List",
+          "Dynamic Programming",
+          "Stack",
+          "Queue",
+          "Binary Search",
+          "Two Pointers",
+        ],
+        trim: true,
+      },
+    ],
+    visibleTestCases: {
+      type: [visibleTestCaseSchema],
+      required: true,
+    },
+    hiddenTestCases: {
+      type: [hiddenTestCaseSchema],
+      required: true,
+    },
+    boilerPlate: {
+      type: [boilerPlateSchema],
+      required: true,
+    },
+    problemCreator: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default problemSchema;
